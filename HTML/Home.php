e6ed18d3d4bd1a9a512e07b2f75d10c8435a2799
FRANCO BERTON <franco.berton93@gmail.com>
2015-11-05 22:33:48 +0100

    Nuovi File

---
+++ HTML/Home.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../CSS/slider.css" />
+    <link rel="stylesheet" href="../CSS/style.css" media="screen">
+    <script type="text/javascript" src="../JS/jquery.js"></script>
+    <script type="text/javascript" src="../JS/jquery.smint.js"></script>
+    <script type="text/javascript">
+	$(document).ready( function() {
+    	 $('.subMenu').smint({'scrollSpeed' : 1000});
+	});
+    </script>
+  
+</head>
+<body>
+<header class="subMenu inner">
+  <h2>Berton Franco</h2>
+  <nav>
+    <ul>
+      <li><a  class="font_helvetica" href="#Home">Home<img src="../IMG/triangolo.png" /></a></li>
+      <li><a  class="font_helvetica" href="#About_me">About me<img src="../IMG/triangolo.png" /></a></li>
+      <li><a  class="font_helvetica" href="#Curriculum">Curriculum<img src="../IMG/triangolo.png" /></a></li>
+      <li><a  class="font_helvetica" href="#Portfolio">Portfolio<img src="../IMG/triangolo.png" /></a></li>
+      <li><a  class="font_helvetica" href="#Contatti">Contatti<img src="../IMG/triangolo.png" /></a></li>
+    </ul>
+  </nav>
+</header>
+<div class="section Home">
+<div id="wowslider-container1">
+	<div class="ws_images">
+	<ul>
+		<li><img src="../IMG/images/foto1.jpg" alt="Software Developer" title="Software Developer" id="wows1_0"/></li>
+		<li><img src="../IMG/images/foto3.jpg" alt="Web developer" title="Web developer" id="wows1_1"/></li>
+		<li><img src="../IMG/images/foto2.jpg" alt="Mobile Application Developer" title="Mobile Application Developer" id="wows1_2"/></li>
+	</ul></div>
+	<div class="ws_bullets"><div>
+		<a href="#" title="Software Developer"><span><img src="../IMG/tooltips/foto1.jpg" alt="Software Developer"/>1</span></a>
+		<a href="#" title="Web developer"><span><img src="../IMG/tooltips/foto3.jpg" alt="Web developer"/>2</span></a>
+		<a href="#" title="Mobile Application Developer"><span><img src="../IMG/tooltips/foto2.jpg" alt="Mobile Application Developer"/>3</span></a>
+	</div></div>
+	</div>	
+</div>
+<div class="section About_me">
+	<h2>About me</h2>
+	<p class="font_helvetica">Mi chiamo Berton Franco, sono un programmatore informatico, ho 22 anni e risiedo a Scorzè in provincia di Venezia. Attualmente, mi trovo al terzo anno del corso di informatica presso l'Università degli studi di Padova. La passione per l'informatica, in particolare per la programmazione, nasce perchè mi ha sempre incuriosito capire come la scrittura del codice si trasformi in azioni generando alla fine un programma. In questi anni di studi ho capito quanto siano importanti le regole basi per la progettazione di un software. L'affidabilità,
+la modificabilità,la comprensibilità e la riusabilità, rappresentando 4 aspetti fondamentali per la creazione di un software qualitativo.
+Secondo il mio punto di vista il programmatore non è un mestiere dove bisogna essere capaci solo a programmare, ma si deve avere una visione futuristica del mercato e dello sviluppo informatico. La mia passione e la mia dedizione cerco sempre di trasmetterla alla committenza, con lo scopo di dare vita ad un software che soddisfi pienamente le esigenze di essa.</p>  
+</div>
+<div class="section Curriculum">
+	<h2>Curriculum</h2>
+	<ul id="ul_curriculum">
+	    <li class="c_tit"><h3 id='h3_green'>Education</h3></li>
+	    <li class="c_desc"><ul class="font_helvetica"><li class="anno">2007-2012</li><li class="desc_anno">Conseguimento Diploma di Ragioniere-Programmatore presso <bold>ITC RICCATI-LUZZATTI</bold> di Treviso</li><li class="anno">2012-presente</li><li class="desc_anno">Università degli studi di Padova <br/>Corso di Informatica</li></ul></li>
+	    <span class="linea"></span>	
+	    <li class="c_tit"><h3 id='h3_blue'>Skills</h3></li>
+	    <li class="c_desc font_helvetica">
+	      <ul class="c_cplusplus" >
+		<li class="skills">C/C++</li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li class="c_per">90%</li>
+	      </ul>
+	      <ul class="c_java">
+		<li class="skills">JAVA</li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li class="c_trasp"></li>
+		<li class="c_per">80%</li>
+	      </ul>
+	      <ul class="c_html">
+		<li class="skills">HTML/HTML5</li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li class="c_per">90%</li>
+	      </ul>
+	      <ul class="c_cplusplus">
+		<li class="skills">CSS</li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li class="c_per">90%</li>
+	      </ul>
+	      <ul class="c_php">
+		<li class="skills">PHP/PERL</li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li class="c_per">90%</li>
+	      </ul>
+	      <ul class="c_XML">
+		<li class="skills">XML/MYSQL</li>
+		<li></li>
+		<li></li>
+		<li class="c_trasp"></li>
+		<li class="c_per">70%</li>
+	      </ul>
+	      <ul class="c_javascript">
+		<li class="skills">JAVASCRIPT/JQUERY</li>
+		<li></li>
+		<li class="c_trasp c_w"></li>
+		<li class="c_per">70%</li>
+	      </ul>
+	      <ul class="c_java">
+		<li class="skills">LINUX</li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li class="c_trasp"></li>
+		<li class="c_per">80%</li>
+	      </ul>
+	    </li>
+	    <span class="linea"></span>	
+	    <li class="c_tit"><h3 id='h3_green'>Language</h3></li>
+	    <li class="c_desc">
+	      <ul class="font_helvetica">
+		<li class="anno">Madre Lingua:</li>
+		<li class="desc_anno"><b>Italiano</b></li>
+		<li class="anno">Altre Lingue Conosciute:</li>
+		<li class="desc_anno"><ul><li><b>Inglese</b>: molto buono(letto,scritto), qualche difficoltà nel parlarlo<br/><b>Francese</b>: scolastico</li></ul></li>
+	      </ul>
+	    </li>
+	</ul>  
+</div>
+<div class="section Portfolio">
+	<h2>Portfolio</h2>
+	<div class="Progetti">
+	<h4>Linqedin</h4>
+	<p class="font_helvetica">Progetto universitario svolto con lo scopo di sviluppare in C++/Qt, un sistema minimale per l’amministrazione ed utilizzo tramite interfaccia utente grafica di un database di contatti professionali ispirato a LinkedIn. </p>
+	<p class="font_helvetica">Un utente LinqedIn è caratterizzato da:</p>
+	<ul class="font_helvetica">
+	  <li>Profilo (ad esempio: dati personali, esperienze professionali, competenze lavorative, lingue conosciute, titoli di studio, etc.).
+	  </li><li>Tipologia dell’account di iscrizione: Basic, Business o Executive.</li>
+	  <li>Rete di contatti professionali con altri utenti Linqedin.</li>
+	</ul>
+
+	<img src="../IMG/home_linq.png"/>
+	<p class="font_helvetica">La mainwindow prevede la possibilità di scegliere di accedere come client oppure come admin<p>
+        <h5>Lato admin</h5>
+	<img src="../IMG/Lato_admin.png" width="90%" />
+	<p class="font_helvetica">Prevede una GUI per la gestione basilare del database degli utenti LinqedIn da parte dell’amministratore. Le funzionalità disponibili sono:</p>
+	<ul class="font_helvetica">
+	  <li>Inserimento nel DB di un nuovo utente LinqedIn; i dati per l’inserimento saranno nome, cognome, codice univoco dell’utente (ad
+esempio, l’email), tipologia di account (Basic, Business, Executive).</li>
+	  <li>Ricerca nel DB di un utente mediante nome/cognome o codice utente.</li>
+	  <li>Rimozione dal DB di un utente LinqedIn.</li>
+	  <li>Cambio di tipologia di account (Basic, Business, Executive) per un utente LinqedIn.</li>
+	  <li>Salvataggio/lettura su file del DB degli utenti LinqedIn.</li>
+	</ul>
+	<h5>Lato client</h5>
+	<img src="../IMG/Lato_Client.png"/>
+	<p class="font_helvetica">Prevede una GUI per l’utilizzo dei servizi LinqedIn da parte di un utente già iscritto a LinqedIn. Le funzionalità disponibili sono:
+	<ul class="font_helvetica"><li>Aggiornamento del proprio profilo.</li><li>Inserimento di un nuovo contatto nella propria rete.</li><li>Rimozione di un contatto dalla propria rete.</li><li>Funzionalità di ricerca sul DB LinqedIn, come autorizzate dalla propria tipologia di account. Le tre tipologie di account permettono delle funzionalità di ricerca crescenti: Basic permette una ricerca che controlla solamente se nel DB LinqedIn esiste un utente con un dato nome e/o cognome; Business permette di ottenere da una ricerca il profilo completo di un utente; Executive permette inoltre di ottenere anche la rete dei contatti di un utente.</li></ul>
+	</div>
+	<div class="Progetti">
+	<h4>NewJob.it</h4>
+	<p></p>
+	</div>
+</div>
+<div class="section Contatti">
+	<h2>Contatti</h2>
+	<form method="post">
+	 <ul>
+	  <li><input type="text" id="Nome" name="Nome" placeholder="Name" /><input type="text" id="Email" name="Email" placeholder="Email" class="mail"/><input type="text" id="Oggetto" name="Oggetto" placeholder="Oggetto" /></li>
+	  <li><textarea value="Message" id="Messaggio" name="Messaggio" placeholder="Message"></textarea></li>
+	  <li><input type="submit" name="submit" class="send width20" value="SEND" /><input type='reset' class="reset width20" value='RESET' /></li>
+	 </ul>
+	</form>
+</div>
+<footer>
+   	<ul>
+  	  <li><p>Made by Franco Berton © 2015</p></li>
+	  <li><a href="https://www.facebook.com/?ref=logo" id="link_fb"><img id="fb" src="../IMG/facebook.png" width="40px"/><img id="fb_hover" src="../IMG/facebook_hover.png" width="40px"/></a><a href="https://instagram.com/frankber93/" id="link_is"><img id="is" src="../IMG/instagram.png"  width="40px" class="soc"/><img  id="is_hover" src="../IMG/instagram_hover.png" class="soc" width="40px"/></a><a href="https://plus.google.com/u/0/110324618546266664276/posts" id="link_google"><img id="google" src="../IMG/google.png"  width="40px" class="soc"/><img id="google_hover" src="../IMG/google_hover.png"  width="40px" class="soc"/></a><a href="https://twitter.com/franco_berton93" id="link_twitter"><img id="twitter" src="../IMG/twitter.png" class="soc" width="40px"/><img id="twitter_hover" src="../IMG/twitter_hover.png"  width="40px" class="soc"/></a></li>
+        </ul>
+</footer>
+<?php
+require_once '/usr/share/php/libphp-phpmailer/class.phpmailer.php'; 
+header('Content-type: text/html;charset=utf-8');
+   function isValidEmail($email){
+   	 return eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email);
+   }
+   if (isset($_POST['submit'])){
+    $email = $_POST['Email'] ;
+    $nome = $_POST['Nome'] ;
+    $oggetto = $_POST['Oggetto'] ;
+    $messaggio = $_POST['Messaggio'] ; 
+    if (empty($nome)){
+    	echo "<script type=\"text/javascript\">
+		document.getElementById('Nome').placeholder = \"Il campo Nome è obbligatorio.\";
+		document.getElementById('Nome').className=\"place_color\";
+	      </script>";}
+    else 
+        {echo "<script type=\"text/javascript\">document.getElementById('Nome').value ='$nome' ;</script>";}
+    if (empty($email)){
+   	echo "<script type=\"text/javascript\">
+		document.getElementById('Email').placeholder = \"Il campo Email è obbligatorio.\";
+		document.getElementById('Email').className+=\" place_color\";
+	     </script>";} 
+    if (!empty($email) && !isValidEmail($email)){
+	echo "<script type='text/javascript'>
+		document.getElementById('Email').placeholder =  \"Inserire un'email valida\";
+		document.getElementById('Email').className+=\" place_color\";
+	      </script>";}
+    if (empty($oggetto)){
+	echo "<script type='text/javascript'>
+		document.getElementById('Oggetto').placeholder = 'Il campo Oggetto è obbligatorio.';
+	        document.getElementById('Oggetto').className+=\"place_color\";
+              </script>";}
+    else 
+        {echo "<script type=\"text/javascript\">
+		document.getElementById('Oggetto').value ='$oggetto' ;
+	       </script>";}
+    if (empty($messaggio)) {
+	echo "<script type='text/javascript'>
+		document.getElementById('Messaggio').placeholder = 'Il campo Messaggio è obbligatorio.';
+		document.getElementById('Messaggio').className+=\"place_color\";
+	      </script>";}
+    else 
+        {echo "<script type=\"text/javascript\">document.getElementById('Messaggio').value ='$messaggio' ;</script>";}
+    if (!empty($nome) && !empty($email) && isValidEmail($email) && !empty($oggetto) && !empty($messaggio)) { 
+		$mail = new PHPMailer();
+		$mail->IsSMTP(); 
+		// L'host remoto con cui spediremo, nel caso di gmail è smtp.gmail.com
+		$mail->Host = "smtp.gmail.com";
+		$mail->SMTPAuth = true;
+		// Specifichiamo che vogliamo spedire tramite ssl, se non volete basta eliminae queste due righe
+		$mail->SMTPSecure = 'ssl';
+		$mail->Port = 465;
+		// Inseriamo i dati della casella che utilizzeremo per spedire
+		$mail->Username = "franco.berton93@gmail.com";
+		$mail->Password = "oliveroliver";
+		// Qui inseriamo l'indirizzo a cui recapitare le email spedite tramite il form
+		$mail->AddAddress("franco.berton93@gmail.com");
+		$mail->AddReplyTo("$email","$nome");
+		// $mail->SMTPDebug = 1;
+		$mail->FromName="$nome";
+		$mail->From = "$email";
+		$mail->Subject = "$oggetto";
+		$mail->Body    = "$messaggio";
+   		if (!$mail->Send())
+  			{echo "<script type='text/javascript'>$(document).ready(function () {alert('Messaggio Non Inviato'.$mail->ErrorInfo);});</script>";}
+                else 
+			{echo "<script type='text/javascript'>
+				document.getElementById('Nome').value ='';
+				document.getElementById('Email').value ='';
+				document.getElementById('Oggetto').value ='';
+				document.getElementById('Messaggio').value ='';
+				$(document).ready(function () {alert('Messaggio Inviato Correttamente');});</script>";}
+		}
+         }
+?>
+</body> 	
+<script type="text/javascript" src="../JS/slider.js"></script>
+<script type="text/javascript" src="../JS/script.js"></script>
+ 
+</html>
